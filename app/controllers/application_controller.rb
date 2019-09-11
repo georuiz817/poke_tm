fb612bb322e6f50d512702fc00e95c4aaf4adc0d
George <georuiz817@gmail.com>
2019-09-11 02:21:32 -0400

    login,logout,signup,sessions complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     
-helpers do
+
     def current_user
         User.find_by_id(session[:user_id])
     end
@@ -9,4 +9,4 @@ helpers do
         !!session[:user_id]
     end
  end
-end
+
