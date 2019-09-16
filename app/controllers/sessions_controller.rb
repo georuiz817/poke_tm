bc72d4b8c2a23615281f498872778f8b3f35a3b3
George <georuiz817@gmail.com>
2019-09-16 19:37:08 -0400

    making edit for pokemon

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      render :new
+      render :login
     end
   end
 
