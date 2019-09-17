f979087e6ebe2d6fedfc1caf4e574b04e233c563
George <georuiz817@gmail.com>
2019-09-17 17:49:11 -0400

    omni working but need to fix it rendering root_path page upon login, still showing not logged in homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
 
-  
   def login 
     @user = User.new
   end
@@ -9,33 +8,35 @@ class SessionsController < ApplicationController
     if auth
       @user = User.find_or_create_by_omniauth(auth)
       session[:user_id] = @user.id
-      redirect_to user_pokemons_path(@user)
+      redirect_to root_path(@user)
     else
       @user = User.find_by(email: user_params[:email])
       if @user && @user.authenticate(user_params[:password])
         session[:user_id] = @user.id
-        redirect_to user_pokemons_path(@user)
+        redirect_to root_path(@user)
       else
-        redirect_to root_path
+        redirect_to login_path
       end
     end
   end
-
+    
+    
 def logout
   session.delete :user_id
   redirect_to root_path
 end
 
 
-  private
-  def auth
-    request.env['omniauth.auth']
-  end
-  
-  def user_params
+private
+def user_params
   params.require(:user).permit(:email, :password)
+end
+
+  def auth
+  request.env['omniauth.auth']
   end
 end
+
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,17 +15,18 @@ class User < ApplicationRecord
     self.email.gsub(/\@.*/, "")
   end 
 
-  
-  
   def self.find_or_create_by_omniauth(auth)
-    user = User.find_by(email: auth['info']['email'])
-
-    if user.nil?
-     user = User.create(email: auth['info']['email'], password: SecureRandom.hex, uid: auth['uid'])
-     elsif user.uid.nil?
-         user.update(uid:auth['uid'])
-     end 
-     user
+    @user = User.find_by(email: auth["info"]["email"])
+    if @user
+      @user.update(uid: auth["uid"])
+    elsif @user.nil?
+      @user = User.create(uid: auth["uid"]) do |u|
+        u.email = auth["info"]["email"]
+        u.password = SecureRandom.hex
+      end
+    end
+    @user
   end
- 
 end
+  
+  
