7a16dc3fefbe6124ee9967e126c8070c4ffb1383
George <georuiz817@gmail.com>
2019-09-16 23:52:04 -0400

    next - complete omni - partials then project complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
   
-
+#you're in the right place
   private
       
         def current_user
