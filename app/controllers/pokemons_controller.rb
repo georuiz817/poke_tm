82b39bd1779eb7045e7ab003a1b3aa9fd125aea9
George <georuiz817@gmail.com>
2019-09-18 11:45:25 -0400

    cleaned up pokemon controller / view

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,8 +1,8 @@
 class PokemonsController < ApplicationController
-  
-  before_action :current_user # @user avlaiable for all actions 
-  before_action :logged_in? #makes sure some1 not logged in can create,view,delete pokemon 
   before_action :redirect_if_not_signed_in
+  before_action :set_user
+  before_action :set_pokemon, except: [:index, :new, :create]
+
   def index #user can view all the pokemon in there group
     @pokemons = @user.pokemons
 end
@@ -26,41 +26,37 @@ def create #created a new pokemon for the specific user, renders errors if any
       end
     end
 
-      def edit
-        if params[:user_id]
-          user = User.find_by(id: params[:user_id])
-          if user.nil?
-            redirect_to root_path, alert: "User not found."
-          else
-            @pokemon = user.pokemons.find_by(id: params[:id])
-            redirect_to user_pokemons_path(@pokemon), alert: "Pokemon not found." if @pokemon.nil?
-          end
-        else
-          @pokemon = Pokemon.find(params[:id])
-        end
-      end
-      
-      def update
-        @pokemon = Pokemon.find(params[:id])
-        if @pokemon.update(pokemon_params)
-         redirect_to user_pokemon_path(@pokemon)
-        else
-          @errors = @pokemon.errors.full_messages
-          render :edit
-        end
-      end
 
+def edit
+  redirect_to user_pokemons_path(current_user) unless current_user == @user
+end
+
+def update
+  if @pokemon.update(pokemon_params)
+    redirect_to pokemon_path(@pokemon)
+  else
+    @errors = @pokemon.errors.full_messages
+    render :edit
+  end
+end
+
+      
 def destroy
-  current_user.pokemons.find(params[:id]).destroy
+  @pokemon.destroy
   redirect_to root_path
 end
 
+private
+    def set_user
+      @user = User.find_by_id(params[:user_id])
+    end
 
-  private
+    def set_pokemon
+      @pokemon = Pokemon.find_by_id(params[:id])
+    end
 
-def pokemon_params
-  params.require(:pokemon).permit(:name, :user_id, :tm_id)
-end
-
-
+    def pokemon_params
+      params.require(:pokemon).permit(:name, :user_id, :tm_id)
+    end
+   
 end
